8fb7dd8d94594e1e7ee4c2b901469a8471c3cad2
Samuraybaba <stormmmae@gmail.com>
2025-04-15 20:53:12 +0400

    Update Ready.js

--- Events/Ready.js
+++ Events/Ready.js
@@ -1,13 +1,20 @@
-
 const { ActivityType } = require("discord.js")
 require("advanced-logs")
 
 module.exports = client => {
 
     console.success(`${client.user.username} adlı hesaba başarıyla bağlanıldı.`)
+
     const statuses = [
-        { name: `${client.user.username} hizmete hazır.`, type: ActivityType.Watching },
-        { name: `Tyial tarafından geliştirilmiştir.`, type: ActivityType.Listening }
+        {
+            name: `${client.user.username} canlı yayında!`,
+            type: ActivityType.Streaming,
+            url: "https://twitch.tv/samurayalone" // Geçerli bir yayın linki gerekiyor
+        },
+        {
+            name: `Ravix Support hizmete hazır.`,
+            type: ActivityType.Listening
+        }
     ];
 
     let index = 0;
@@ -15,7 +22,11 @@ module.exports = client => {
     setInterval(() => {
         const status = statuses[index];
         client.user.setPresence({
-            activities: [{ name: status.name, type: status.type }],
+            activities: [{
+                name: status.name,
+                type: status.type,
+                url: status.url || undefined
+            }],
             status: 'online'
         });
 
